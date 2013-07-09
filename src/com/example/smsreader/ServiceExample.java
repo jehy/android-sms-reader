9f247e480acdfee6dd60ffa33f1b3db23ba48c11
Jehy <fate@jehy.ru>
2013-07-10 03:47:03 +0400

    changed api call to Roma mode

--- src/com/example/smsreader/ServiceExample.java
+++ src/com/example/smsreader/ServiceExample.java
@@ -1,6 +1,5 @@
 package com.example.smsreader;
 
-
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
@@ -38,17 +37,18 @@ public class ServiceExample extends IntentService {
 		super(name);
 		Log.v("Service create", "creating service");
 	}
+
 	public ServiceExample() {
 		super(null);
 		Log.v("Service create", "creating service");
 	}
-/*
-	public ServiceExample() {
-		super("com.example.smsinformer.ServiceExample");
-		Log.v("Service create", "creating service");
-	}
-*/
-	//CentreonMessages messages = new CentreonMessages();
+
+	/*
+	 * public ServiceExample() {
+	 * super("com.example.smsinformer.ServiceExample"); Log.v("Service create",
+	 * "creating service"); }
+	 */
+	// CentreonMessages messages = new CentreonMessages();
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -99,177 +99,151 @@ public class ServiceExample extends IntentService {
 
 	private class ThreadDemo extends Thread {
 		Context serviceContext;
-		public ThreadDemo(Context context)
-		{
-			serviceContext=context;
+
+		public ThreadDemo(Context context) {
+			serviceContext = context;
 		}
+
 		@Override
 		public void run() {
 			super.run();
 			Log.v("Thread service", "Running service thread");
-			//boolean s = ReadMessages();
-			long LastSentId=GetLastSentId();
-			ArrayList<SmsMmsMessage> Messages=SmsPopupUtils.getMessages(serviceContext,1);
-			long LastSmsId=Messages.get(0).getMessageId();
+			// boolean s = ReadMessages();
+			long LastSentId = GetLastSentId();
+			ArrayList<SmsMmsMessage> Messages = SmsPopupUtils.getMessages(
+					serviceContext, 1);
+			long LastSmsId = Messages.get(0).getMessageId();
 
-			Log.e("SmsReader","Last received message ID is "+LastSmsId);
-			
-			if(LastSentId>LastSmsId)
-			{
-				Log.e("SmsReader Error","SMS id was reset. Setting it to last message");
+			Log.e("SmsReader", "Last received message ID is " + LastSmsId);
+
+			if (LastSentId > LastSmsId) {
+				Log.e("SmsReader Error",
+						"SMS id was reset. Setting it to last message");
 				SetLastSentId(LastSmsId);
 			}
-			if(LastSmsId==0)
-			{
-				Log.e("SmsReader Error","LastSmsId was zero. Setting it to last message to avoid spam");
+			if (LastSmsId == 0) {
+				Log.e("SmsReader Error",
+						"LastSmsId was zero. Setting it to last message to avoid spam");
 				SetLastSentId(LastSmsId);
 			}
-		
-			for(int i=Messages.size()-1;i>=0;i--)//begin from the oldest
+
+			for (int i = Messages.size() - 1; i >= 0; i--)// begin from the
+															// oldest
 			{
-				long messageId=Messages.get(i).getMessageId();
-				Log.e("Message id",String.valueOf(messageId));
-				Log.e("Message time",String.valueOf(Messages.get(i).getTimestamp()));
-				Log.e("Message sender",Messages.get(i).getAddress());
-				Log.e("Message text",Messages.get(i).getMessageBody());
-				String source="+79651249856";
-				if(Messages.get(i).getAddress().compareTo(source)==0)
-				{
-					Log.e("Sms Reader","Our guy's message!");
-					if(LastSentId>=messageId)
-					{
-						Log.e("SmsReader","Old message");
-					}
-					else
-					{
-						Log.e("SmsReader","New message! Need to pass it!");
-						//TODO: parse message text and send it to server. If all ok - update last sent id
-						String url="http://forest.eias.ru/state.php?id=1&state=2&time=92387412";
-						   HttpClient httpclient = new DefaultHttpClient();
-						   try
-						   {
-						    HttpResponse response = httpclient.execute(new HttpGet(url));
-						    StatusLine statusLine = response.getStatusLine();
-						    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-						        ByteArrayOutputStream out = new ByteArrayOutputStream();
-						        response.getEntity().writeTo(out);
-						        out.close();
-						        String responseString = out.toString();
-						        Log.e("SmsReader",responseString);
-						        //TODO: check that it is really ok
-						        Log.e("SmsReader","Setting LastSentId "+ messageId);
-						        SetLastSentId(messageId);
-						        //..more logic
-						    } else{
-						        //Closes the connection.
-						        response.getEntity().getContent().close();
-						        throw new IOException(statusLine.getReasonPhrase());
-						    }
-						   }
-						   catch (IOException e) {
-							   Log.e("SmsReader","Something fucked up http request...");
-					            //TODO Handle problems..
-					        } 
-					
+				long messageId = Messages.get(i).getMessageId();
+				Log.e("Message id", String.valueOf(messageId));
+				Log.e("Message time",
+						String.valueOf(Messages.get(i).getTimestamp()));
+				Log.e("Message sender", Messages.get(i).getAddress());
+				Log.e("Message text", Messages.get(i).getMessageBody());
+				String source = "+79651249856";// only from Bond work phone
+				if (Messages.get(i).getAddress().compareTo(source) == 0) {
+					Log.e("Sms Reader", "Our guy's message!");
+					if (LastSentId >= messageId) {
+						Log.e("SmsReader", "Old message");
+					} else {
+						Log.e("SmsReader", "New message! Need to pass it!");
+						// TODO: parse message text and send it to server. If
+						// all ok - update last sent id
+
+						/**
+						 * /api.php?mode=create_event
+						 * 
+						 * @param id_sensor
+						 * @param state
+						 * @param time
+						 * @return json {status:'',data:''}
+						 */
+
+						String url = "http://forest.eias.ru/api.php?mode=create_event&id_sensor=1&state=1&time=121212";
+						HttpClient httpclient = new DefaultHttpClient();
+						try {
+							HttpResponse response = httpclient
+									.execute(new HttpGet(url));
+							StatusLine statusLine = response.getStatusLine();
+							if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+								ByteArrayOutputStream out = new ByteArrayOutputStream();
+								response.getEntity().writeTo(out);
+								out.close();
+								String responseString = out.toString();
+								Log.e("SmsReader", responseString);
+								// TODO: check that it is really ok
+								Log.e("SmsReader", "Setting LastSentId "
+										+ messageId);
+								SetLastSentId(messageId);
+								// ..more logic
+							} else {
+								// Closes the connection.
+								response.getEntity().getContent().close();
+								throw new IOException(
+										statusLine.getReasonPhrase());
+							}
+						} catch (IOException e) {
+							Log.e("SmsReader",
+									"Something fucked up http request...");
+							// TODO Handle problems..
+						}
+
 					}
 				}
 			}
-			
-			/*long time = GetLastTime();
-			Log.v("Thread service", "Searching for messages modified after "
-					+ time);
-			
-			CentreonMessage m;
-			if (!s)
-				Log.v("Thread service", "ReadMessages call failed, aborting");
-			else
-				while ((m = messages.getNew(time)) != null) {
-					SmsManager smsManager = SmsManager.getDefault();
-					Log.v("Thread service", "Sending sms to " + m.phone);
-					String message = m.getSmsText();
-					Log.v("Thread service", "SMS text:  " + message);
 
-					String phone = m.phone;
-					// smsManager.sendMultipartTextMessage(phone, null,
-					// msgTexts, listOfIntents, null);
-					smsManager
-							.sendTextMessage(phone, null, message, null, null);
-					SetLastTime(m.time);
-					time = m.time;
-					Log.v("Thread service",
-							"Setting last modification time to " + m.time);
-				}
-		*/}
+			/*
+			 * long time = GetLastTime(); Log.v("Thread service",
+			 * "Searching for messages modified after " + time);
+			 * 
+			 * CentreonMessage m; if (!s) Log.v("Thread service",
+			 * "ReadMessages call failed, aborting"); else while ((m =
+			 * messages.getNew(time)) != null) { SmsManager smsManager =
+			 * SmsManager.getDefault(); Log.v("Thread service",
+			 * "Sending sms to " + m.phone); String message = m.getSmsText();
+			 * Log.v("Thread service", "SMS text:  " + message);
+			 * 
+			 * String phone = m.phone; //
+			 * smsManager.sendMultipartTextMessage(phone, null, // msgTexts,
+			 * listOfIntents, null); smsManager .sendTextMessage(phone, null,
+			 * message, null, null); SetLastTime(m.time); time = m.time;
+			 * Log.v("Thread service", "Setting last modification time to " +
+			 * m.time); }
+			 */}
 
-			/*}
-
-	boolean ReadMessages() {
-
-		final Runtime runtime = Runtime.getRuntime();
-		if (REMOUNT) {
-			Log.v("ReadMessages", "Remounting SD card to update contents");
-
-			String[] str = { "su", "-c", "umount /mnt/sdcard" };
-			String[] str2 = { "su", "-c",
-					"mount -r -t vfat /dev/block/vold/179:1 /mnt/sdcard" };
-			try {
-				runtime.exec(str);
-				runtime.exec(str2);
-			} catch (IOException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			}
-		}
-		Log.v("ReadMessages", "Copying messages log to local app folder");
-		String from = Environment.getExternalStorageDirectory()
-				+ "/SmsInformer/messages.txt";
-		PackageManager m = getPackageManager();
-		String s = getPackageName();
-		PackageInfo p = null;
-		try {
-			p = m.getPackageInfo(s, 0);
-		} catch (NameNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		s = p.applicationInfo.dataDir;
-
-		String to = s + "/messages.txt";
-		String command = "cp " + from + " " + to;
-		Log.v("ReadMessages", command);
-
-		String[] str3 = { "su", "-c", command };
-		try {
-			runtime.exec(str3);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		Log.v("ReadMessages", "Reading messages file");
-		BufferedReader rd;
-		StringBuilder sb = new StringBuilder();
-		try {
-			rd = new BufferedReader(new FileReader(to));
-			String line = null;
-			// read all the lines till the end
-			while ((line = rd.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			rd.close(); // close reader
-			if (sb.length() > 5) {
-				messages.set(sb.toString());
-				return true;
-			} else {
-				Log.v("ReadMessages",
-						"Too little messages length, won't parse.");
-				return false;
-			}
-		} catch (Exception e) {
-			// Log.e("Service run", e.getMessage());
-			e.printStackTrace();
-			return false;
-		}*/
+		/*
+		 * }
+		 * 
+		 * boolean ReadMessages() {
+		 * 
+		 * final Runtime runtime = Runtime.getRuntime(); if (REMOUNT) {
+		 * Log.v("ReadMessages", "Remounting SD card to update contents");
+		 * 
+		 * String[] str = { "su", "-c", "umount /mnt/sdcard" }; String[] str2 =
+		 * { "su", "-c", "mount -r -t vfat /dev/block/vold/179:1 /mnt/sdcard" };
+		 * try { runtime.exec(str); runtime.exec(str2); } catch (IOException e2)
+		 * { // TODO Auto-generated catch block e2.printStackTrace(); } }
+		 * Log.v("ReadMessages", "Copying messages log to local app folder");
+		 * String from = Environment.getExternalStorageDirectory() +
+		 * "/SmsInformer/messages.txt"; PackageManager m = getPackageManager();
+		 * String s = getPackageName(); PackageInfo p = null; try { p =
+		 * m.getPackageInfo(s, 0); } catch (NameNotFoundException e1) { // TODO
+		 * Auto-generated catch block e1.printStackTrace(); } s =
+		 * p.applicationInfo.dataDir;
+		 * 
+		 * String to = s + "/messages.txt"; String command = "cp " + from + " "
+		 * + to; Log.v("ReadMessages", command);
+		 * 
+		 * String[] str3 = { "su", "-c", command }; try { runtime.exec(str3); }
+		 * catch (IOException e1) { // TODO Auto-generated catch block
+		 * e1.printStackTrace(); }
+		 * 
+		 * Log.v("ReadMessages", "Reading messages file"); BufferedReader rd;
+		 * StringBuilder sb = new StringBuilder(); try { rd = new
+		 * BufferedReader(new FileReader(to)); String line = null; // read all
+		 * the lines till the end while ((line = rd.readLine()) != null) {
+		 * sb.append(line + "\n"); } rd.close(); // close reader if (sb.length()
+		 * > 5) { messages.set(sb.toString()); return true; } else {
+		 * Log.v("ReadMessages", "Too little messages length, won't parse.");
+		 * return false; } } catch (Exception e) { // Log.e("Service run",
+		 * e.getMessage()); e.printStackTrace(); return false; }
+		 */
 	}
 
 	long GetLastSentId() {
