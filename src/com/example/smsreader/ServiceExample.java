dfd17aaaba15d53cbecdc5b00ad784c752b5a5f7
Jehy <fate@jehy.ru>
2013-07-10 11:21:18 +0400

    break on error

--- src/com/example/smsreader/ServiceExample.java
+++ src/com/example/smsreader/ServiceExample.java
@@ -1,8 +1,6 @@
 package com.example.smsreader;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -20,12 +18,7 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Environment;
 import android.os.IBinder;
-import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -175,75 +168,22 @@ public class ServiceExample extends IntentService {
 							} else {
 								// Closes the connection.
 								response.getEntity().getContent().close();
-								throw new IOException(
-										statusLine.getReasonPhrase());
+								//throw new IOException(
+								//		statusLine.getReasonPhrase());
+								Log.e("SmsREader",statusLine.getReasonPhrase());
+								break;
 							}
 						} catch (IOException e) {
 							Log.e("SmsReader",
 									"Something fucked up http request...");
+							break;
 							// TODO Handle problems..
 						}
 
 					}
 				}
 			}
-
-			/*
-			 * long time = GetLastTime(); Log.v("Thread service",
-			 * "Searching for messages modified after " + time);
-			 * 
-			 * CentreonMessage m; if (!s) Log.v("Thread service",
-			 * "ReadMessages call failed, aborting"); else while ((m =
-			 * messages.getNew(time)) != null) { SmsManager smsManager =
-			 * SmsManager.getDefault(); Log.v("Thread service",
-			 * "Sending sms to " + m.phone); String message = m.getSmsText();
-			 * Log.v("Thread service", "SMS text:  " + message);
-			 * 
-			 * String phone = m.phone; //
-			 * smsManager.sendMultipartTextMessage(phone, null, // msgTexts,
-			 * listOfIntents, null); smsManager .sendTextMessage(phone, null,
-			 * message, null, null); SetLastTime(m.time); time = m.time;
-			 * Log.v("Thread service", "Setting last modification time to " +
-			 * m.time); }
-			 */}
-
-		/*
-		 * }
-		 * 
-		 * boolean ReadMessages() {
-		 * 
-		 * final Runtime runtime = Runtime.getRuntime(); if (REMOUNT) {
-		 * Log.v("ReadMessages", "Remounting SD card to update contents");
-		 * 
-		 * String[] str = { "su", "-c", "umount /mnt/sdcard" }; String[] str2 =
-		 * { "su", "-c", "mount -r -t vfat /dev/block/vold/179:1 /mnt/sdcard" };
-		 * try { runtime.exec(str); runtime.exec(str2); } catch (IOException e2)
-		 * { // TODO Auto-generated catch block e2.printStackTrace(); } }
-		 * Log.v("ReadMessages", "Copying messages log to local app folder");
-		 * String from = Environment.getExternalStorageDirectory() +
-		 * "/SmsInformer/messages.txt"; PackageManager m = getPackageManager();
-		 * String s = getPackageName(); PackageInfo p = null; try { p =
-		 * m.getPackageInfo(s, 0); } catch (NameNotFoundException e1) { // TODO
-		 * Auto-generated catch block e1.printStackTrace(); } s =
-		 * p.applicationInfo.dataDir;
-		 * 
-		 * String to = s + "/messages.txt"; String command = "cp " + from + " "
-		 * + to; Log.v("ReadMessages", command);
-		 * 
-		 * String[] str3 = { "su", "-c", command }; try { runtime.exec(str3); }
-		 * catch (IOException e1) { // TODO Auto-generated catch block
-		 * e1.printStackTrace(); }
-		 * 
-		 * Log.v("ReadMessages", "Reading messages file"); BufferedReader rd;
-		 * StringBuilder sb = new StringBuilder(); try { rd = new
-		 * BufferedReader(new FileReader(to)); String line = null; // read all
-		 * the lines till the end while ((line = rd.readLine()) != null) {
-		 * sb.append(line + "\n"); } rd.close(); // close reader if (sb.length()
-		 * > 5) { messages.set(sb.toString()); return true; } else {
-		 * Log.v("ReadMessages", "Too little messages length, won't parse.");
-		 * return false; } } catch (Exception e) { // Log.e("Service run",
-		 * e.getMessage()); e.printStackTrace(); return false; }
-		 */
+		}
 	}
 
 	long GetLastSentId() {
